2929adb7d637c4b30cc3df4494fccb1d786c42e6
HakeemWashington <hakeemgeorgewashington@gmail.com>
2019-09-26 09:22:03 -0400

    updating files

--- airbnb-back-end/app.js
+++ airbnb-back-end/app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const helmet = require('helmet')
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hostRouter = require('./routes/host');
 
 var app = express();
-app.use(helmet())
+app.use(helmet());
 
 // Allow cross-origin.....
 app.use(function(req, res, next) {
@@ -16,7 +17,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
-  });
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,5 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/host', hostRouter);
 
 module.exports = app;
