6c3bd58ecab77eaaa47356b5b3214bda853c39f9
HakeemWashington <hakeemgeorgewashington@gmail.com>
2019-09-30 09:13:13 -0400

    updating files

--- airbnb-back-end/routes/index.js
+++ airbnb-back-end/routes/index.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer')
+var upload = multer({ dest: './public/images' })
+const db = require('../db');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(req.get('host'))
-  console.log(req.headers.host);
-  console.log(req.hostname);
-  res.json({});
-});
+
+router.post('*',
+  upload.single('locationImage'),
+  (req, res, next)=>{
+    const token = req.body.token
+    const getUserIdQuery = `SELECT id FROM users WHERE token = ?`;
+    db.query(getUserIdQuery,[token], (err, results)=>{
+        if(results.length === 0){
+            res.locals.loggedIn = false;
+        }else{
+          res.locals.loggedIn = true;
+          res.locals.uid = results[0].id
+        }
+        next(); //send user on to next route
+    })
+})
+
+
+router.get('/cities',(req, res, next)=>{
+  const citiesQuery = `SELECT * FROM cities
+    ORDER BY RAND()
+    LIMIT 8`
+  db.query(citiesQuery,(err, results)=>{
+    if(err) throw err;
+    res.json(results);
+  })
+})
+
 
 module.exports = router;
